bd9e444e137c935d8b600b8af1233f16382b46e4
AQ <bluegrass@abv.bg>
2015-11-08 14:54:09 +0000

    php testing

--- index.php
+++ index.php
@@ -5,16 +5,9 @@
 <body>
 <p>
     <?php
-    echo "Hello World! "."\n";
-    echo "Hello," . " " . "wordls" . "!" . "\n";
-    echo 5*7 . "\n";
-    $myname = "Alex Quest";
-    $myage = 22;
-    echo "My name is " . $myname. " and my age is " . $myage;
-    if($myname == "Alex Quest"){
-        print "Hello matey :)";
-    }else{
-        print "Who are you?";
+    $provisions = array("specs","mugs","sausage rolls");
+    foreach($provisions as $x){
+        print "<p>$x</p>";
     }
     ?>
 </p>
