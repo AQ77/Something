4304af219111084b269523374012cd8d7d4ce6e3
1306248 <bluegrass@abv.bg>
2015-11-09 11:15:16 +0000

    db

--- helloSelector.php
+++ helloSelector.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1306248
- * Date: 09/11/2015
- * Time: 10:33
- */
+    $db = new mysqli(
+        "eu-cdbr-azure-north-d.cloudapp.net",
+        "b6493667ce3e8d",
+        "1be52bfc",
+        "firstwebdatabase"
+
+    );
+
+    if($db->connect_errno){
+        die('Connectfailed['.$db->connect_error.']');
+    }else{
+        echo "good job!";
+    }
+?>
